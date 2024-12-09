27400254a3cfb76935399e5634b7e48cd1b701ec
Obed Okemsinachi <obedokemsinachi@gmail.com>
2024-12-10 00:53:40 +0100

    tw patch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ],
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
       fontFamily: {
